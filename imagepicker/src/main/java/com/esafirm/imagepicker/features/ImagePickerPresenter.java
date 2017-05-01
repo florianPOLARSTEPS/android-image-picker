cb1294e5e7063e907c0266aaa8ff2e8f0c2955b8
Florian Grashaeftl <florian@polarsteps.com>
2017-05-01 12:09:21 +0200

    try to take readable URI permissions.

--- imagepicker/src/main/java/com/esafirm/imagepicker/features/ImagePickerPresenter.java
+++ imagepicker/src/main/java/com/esafirm/imagepicker/features/ImagePickerPresenter.java
@@ -2,6 +2,7 @@ package com.esafirm.imagepicker.features;
 
 import android.app.Activity;
 import android.content.ClipData;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -9,6 +10,7 @@ import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.esafirm.imagepicker.R;
@@ -27,10 +29,13 @@ import java.util.List;
 
 public class ImagePickerPresenter extends BasePresenter<ImagePickerView> {
 
-    public FileSystemData fileSystemData;
-    private ImageLoader imageLoader;
+    public  FileSystemData fileSystemData;
+
     private CameraModule cameraModule = new DefaultCameraModule();
-    private Handler handler = new Handler(Looper.getMainLooper());
+
+    private Handler      handler      = new Handler(Looper.getMainLooper());
+
+    private ImageLoader    imageLoader;
 
     public ImagePickerPresenter(ImageLoader imageLoader) {
         this.imageLoader = imageLoader;
@@ -110,7 +115,8 @@ public class ImagePickerPresenter extends BasePresenter<ImagePickerView> {
         Context context = activity.getApplicationContext();
         Intent intent = cameraModule.getCameraIntent(activity, config);
         if (intent == null) {
-            Toast.makeText(context, context.getString(R.string.ef_error_create_image_file), Toast.LENGTH_LONG).show();
+            Toast.makeText(context, context.getString(R.string.ef_error_create_image_file), Toast.LENGTH_LONG)
+                    .show();
             return;
         }
         activity.startActivityForResult(intent, requestCode);
@@ -127,7 +133,8 @@ public class ImagePickerPresenter extends BasePresenter<ImagePickerView> {
             if (data.getClipData() != null) {
                 ClipData mClipData = data.getClipData();
                 for (int i = 0; i < mClipData.getItemCount(); i++) {
-                    Uri uri = mClipData.getItemAt(i).getUri();
+                    Uri uri = mClipData.getItemAt(i)
+                            .getUri();
                     if (!images.contains(uri)) {
                         images.add(uri);
                     }
@@ -136,6 +143,19 @@ public class ImagePickerPresenter extends BasePresenter<ImagePickerView> {
         }
 
         if (images.size() > 0) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                final int takeFlags = data.getFlags() & Intent.FLAG_GRANT_READ_URI_PERMISSION;
+                ContentResolver resolver = context.getContentResolver();
+                for (Uri uri : images) {
+                    try {
+                        resolver.takePersistableUriPermission(uri, takeFlags);
+                    } catch (Exception e) {
+                        Log.e("ImagePicker", "Could not receive permissions", e);
+                    }
+                }
+            }
+
             imageLoader.loadExternalDeviceImages(config, images, new ImageLoaderListener() {
                 @Override
                 public void onImageLoaded(final List<Image> images, List<Folder> folders, final FileSystemData mFileSystemData) {
